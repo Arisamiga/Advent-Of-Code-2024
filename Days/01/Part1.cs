8289159ce318714e3487febb1219be5ca74af380
Aris <64918822+Arisamiga@users.noreply.github.com>
2024-12-02 23:10:16 +0000

    Day 01

---
+++ Days/01/Part1.cs
@@ -0,0 +1,35 @@
+﻿namespace _01
+{
+    internal class Part1
+    {
+        static void Main(string[] args)
+        {
+
+            String [] values = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "value.txt"));
+
+            List<string> left = new List<string>();
+            List<string> right = new List<string>();
+
+            foreach (String value in values)
+            {
+                var locations = value.Split(" ");
+                locations = locations.Where(a => !string.IsNullOrWhiteSpace(a)).ToArray();
+                left.Add(locations[0]);
+                right.Add(locations[1]);
+            };
+            right.Sort();
+            left.Sort();
+
+            var TotalNum = 0;
+            for (int i = 0; i < right.Count; i++)
+            {
+                var leftV = Int32.Parse(left[i]);
+                var rightV = Int32.Parse(right[i]);
+
+                TotalNum += Math.Abs(leftV - rightV);
+            }
+
+            Console.WriteLine("Total: " +  TotalNum);
+        }
+    }
+}
---
+++ Days/01/Part2.cs
@@ -0,0 +1,32 @@
+﻿namespace _01
+{
+    internal class Part2
+    {
+        static void Main(string[] args)
+        {
+
+            String[] values = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "value.txt"));
+
+            List<string> left = new List<string>();
+            List<string> right = new List<string>();
+            var totalNum = 0;
+
+            foreach (String value in values)
+            {
+                var locations = value.Split(" ");
+                locations = locations.Where(a => !string.IsNullOrWhiteSpace(a)).ToArray();
+                left.Add(locations[0]);
+                right.Add(locations[1]);
+            };
+
+
+            foreach (String leftV in left)
+            {
+                var counts = right.Where(a => a == leftV).ToArray();
+                totalNum += Int32.Parse(leftV) * counts.Length;
+                
+            };
+            Console.WriteLine(totalNum);
+        }
+    }
+}
