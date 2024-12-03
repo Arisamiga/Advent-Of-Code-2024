2d6c7b41522e1543730bd245c40e13a1a453ce7b
Aris <64918822+Arisamiga@users.noreply.github.com>
2024-12-03 19:50:43 +0000

    Day 2

---
+++ Days/02/Part1.cs
@@ -0,0 +1,76 @@
+﻿namespace _02
+{
+    internal class Part1
+    {
+        static void Main2(string[] args)
+        {
+
+            String[] values = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "value.txt"));
+
+            
+
+            int safeSpots = 0;
+            foreach (String value in values)
+            {
+                int safeness = 0; // 0 = unsafe 1=safe
+                var locations = value.Split(" ");
+                locations = locations.Where(a => !string.IsNullOrWhiteSpace(a)).ToArray();
+                int previousNum = -1;
+                var state = "null"; 
+                foreach (String location in locations)
+                {
+                    if (previousNum == -1)
+                    {
+                        previousNum = Int32.Parse(location);
+                        continue;
+                    }
+                    if (Int32.Parse(location) > previousNum)
+                    {
+                        if (state != "Increase" && state != "null")
+                        {
+                            //no
+                            //Console.WriteLine(Int32.Parse(location) + " / " + previousNum);
+                            safeness = 0;
+                            break;
+                        }
+
+                        state = "Increase";
+                    }
+                    else if (Int32.Parse(location) < previousNum)
+                    {
+                        if (state != "Decreases" && state != "null")
+                        {
+                            //no
+                            //Console.WriteLine(Int32.Parse(location) + " | " +  previousNum);
+                            safeness = 0;
+                            break;
+                        }
+                        state = "Decreases";
+                    }
+
+                    int change = Math.Abs(Int32.Parse(location) - previousNum);
+                    //Console.WriteLine("Change: "+ change);
+
+                    // Suddent Change check
+                    if (change > 3 || change == 0)
+                    {
+                        //uNSAFE
+                        safeness = 0;
+                        break;
+                    }
+
+                    safeness = 1;
+                    previousNum = Int32.Parse(location);
+                }
+                //Console.WriteLine(safeness);
+                if (safeness != 0)
+                {
+                    safeSpots++;
+                    safeness = 0;
+                }
+            };
+
+            Console.WriteLine(safeSpots);
+        }
+    }
+}
---
+++ Days/02/Part2.cs
@@ -0,0 +1,76 @@
+﻿namespace _02
+{
+    internal class Part2
+    {
+        static void Main(string[] args)
+        {
+            String[] values = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "value.txt"));
+            Part2 part2 = new Part2();
+            int safeSpots = 0;
+            for (int e = 0; e < values.Length; e++)
+            {
+                var value = values[e];
+                var report = value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+               
+                if (part2.isSafe(report))
+                {
+                    safeSpots++;
+                }
+                else
+                {
+                    for (int i = 0; i < report.Count; i++)
+                    {
+                        var reportCopy = report.ToList();
+                        reportCopy.RemoveAt(i);
+                        if (part2.isSafe(reportCopy))
+                        {
+                            safeSpots++;
+                            break;
+                        }
+                    }
+
+                }
+            }
+
+            Console.WriteLine(safeSpots);
+        }
+        public bool isSafe(List<int> locations)
+        {
+            if (locations.Count < 2)
+            {
+                return true;
+            }
+
+            var firstChange = locations[1] - locations[0];
+
+            if (firstChange == 0 || Math.Abs(firstChange) > 3)
+            {
+                return false;
+            }
+
+            var direction = firstChange / Math.Abs(firstChange);
+
+            for (int i = 1; i < locations.Count - 1; i++)
+            {
+
+                var change = locations[i + 1] - locations[i];
+                //Console.WriteLine("Change: " + change);
+
+                // Sudden Change check
+                if (change == 0 || Math.Abs(change) > 3)
+                {
+                    return false;
+                }
+
+                var InDirection = change / Math.Abs(change);
+
+                if (InDirection != direction)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
