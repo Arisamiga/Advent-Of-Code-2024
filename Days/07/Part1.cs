64e32243d03a1dcb6e6d07043829399c922062a7
Aris <64918822+Arisamiga@users.noreply.github.com>
2024-12-08 16:45:20 +0000

    Day 07 Woooo (This was nice)

---
+++ Days/07/Part1.cs
@@ -0,0 +1,45 @@
+﻿namespace _07
+{
+    internal class Part1
+    {
+        static public bool Solve (ulong result, int index, ulong expected, ulong[] numbers)
+        {
+           if (result > expected)
+            {
+                return false;
+            }
+            if (index == numbers.Length)
+            {
+                return result == expected;
+            }
+            return Solve(result + numbers[index], index + 1, expected, numbers) || Solve(result * numbers[index], index + 1, expected, numbers);
+        }
+
+        static public bool ableToSolve(ulong[] numbers, ulong expected)
+        {
+            return Solve(numbers[0], 1, expected, numbers);
+        }
+        static void Main(string[] args)
+        {
+
+            String[] values = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "value.txt"));
+            ulong total = 0;
+            foreach (String value in values)
+            {
+                var locations = value.Split(" ");
+                locations = locations.Where(a => !string.IsNullOrWhiteSpace(a)).ToArray();
+                locations = locations.Select(a => a.Replace(":", "")).ToArray();
+                
+                var expected = ulong.Parse(locations[0].Trim());
+
+                var numbers = locations.Skip(1).Select(a => ulong.Parse(a)).ToArray();
+                
+                if (ableToSolve(numbers, expected))
+                {
+                    total += expected;
+                }
+            };
+            Console.WriteLine(total);
+        }
+    }
+}
