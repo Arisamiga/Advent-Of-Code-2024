af087fffeaae7201547502620b6f972f2eefe7dd
Aris <64918822+Arisamiga@users.noreply.github.com>
2024-12-14 19:56:26 +0000

    Day 11

---
+++ Days/11/Part1.cs
@@ -0,0 +1,60 @@
+﻿namespace _11
+{
+    internal class Part1
+    {
+        public static List<ulong> Blink(List<ulong> rocksList)
+        {
+            List<ulong> rocks = rocksList;
+            for (int i = 0; i < rocks.Count; i++)
+            {
+
+                if (rocks[i] < 0)
+                {
+                    Console.WriteLine("Rock is empty");
+                }
+                if (rocks[i] == 0)
+                {
+                    rocks[i] = 1;
+                }
+                else if (rocks[i].ToString().Length % 2 == 0)
+                {
+                    string rockStr = rocks[i].ToString();
+                    int mid = rockStr.Length / 2;
+                    //Console.WriteLine(rocks[i].ToString());
+                    int leftHalf = int.Parse(rockStr.Substring(0, mid));
+                    int rightHalf = int.Parse(rockStr.Substring(mid));
+                    rocks[i] = (ulong)leftHalf;
+                    rocks.Insert(i + 1, (ulong)rightHalf);
+                    i++;
+                }
+                else
+                {
+                    if (rocks[i] * 2024 < 0)
+                    {
+                        Console.WriteLine("OVERFLOW");
+                    }
+                    rocks[i] = rocks[i] * 2024;
+                }
+            }
+            return rocks;
+        }
+
+        static void Main(string[] args)
+        {
+            String[] values = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "value.txt"));
+
+            List<ulong> rocks = values[0].Split(" ").Select(ulong.Parse).ToList();
+            
+            for (int i = 0; i < 25; i++)
+            {
+                rocks = Blink(rocks);
+            }
+
+            // Remove any empty rocks
+            rocks.RemoveAll(x => x < 0);
+
+            Console.WriteLine(rocks.Count());
+
+        }
+    }
+}
---
+++ Days/11/Part2.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+
+namespace _11
+{
+    internal class Part2
+    {
+        public static Dictionary<ulong, ulong> Blink(Dictionary<ulong, ulong> rocksList)
+        {
+            Dictionary<ulong, ulong> rocks = new Dictionary<ulong, ulong>();
+            foreach (var kvp in rocksList)
+            {
+                ulong rock = kvp.Key;
+                ulong occurrences = kvp.Value;
+
+                if (rock == 0)
+                {
+                    if (rocks.ContainsKey(1))
+                    {
+                        rocks[1] += occurrences;
+                    }
+                    else
+                    {
+                        rocks[1] = occurrences;
+                    }
+                }
+                else if (rock.ToString().Length % 2 == 0)
+                {
+                    string rockStr = rock.ToString();
+                    int mid = rockStr.Length / 2;
+                    ulong leftHalf = ulong.Parse(rockStr.Substring(0, mid));
+                    ulong rightHalf = ulong.Parse(rockStr.Substring(mid));
+
+                    if (rocks.ContainsKey(leftHalf))
+                    {
+                        rocks[leftHalf] += occurrences;
+                    }
+                    else
+                    {
+                        rocks[leftHalf] = occurrences;
+                    }
+
+                    if (rocks.ContainsKey(rightHalf))
+                    {
+                        rocks[rightHalf] += occurrences;
+                    }
+                    else
+                    {
+                        rocks[rightHalf] = occurrences;
+                    }
+                }
+                else
+                {
+                    ulong newRock = rock * 2024;
+                    if (newRock < rock) // Check for overflow
+                    {
+                        Console.WriteLine("OVERFLOW");
+                    }
+
+                    if (rocks.ContainsKey(newRock))
+                    {
+                        rocks[newRock] += occurrences;
+                    }
+                    else
+                    {
+                        rocks[newRock] = occurrences;
+                    }
+                }
+            }
+            return rocks;
+        }
+
+        static void Main(string[] args)
+        {
+            String[] values = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "value.txt"));
+
+            Dictionary<ulong, ulong> initialRocks = values[0].Split(" ").Select(ulong.Parse).ToDictionary(x => x, x => 1UL);
+
+            for (int i = 0; i < 75; i++)
+            {
+                initialRocks = Blink(initialRocks);
+            }
+
+            // Get all occurrences and add them together
+            ulong totalRocks = initialRocks.Values.Aggregate((x, y) => x + y);
+
+            Console.WriteLine(totalRocks);
+        }
+    }
+}
